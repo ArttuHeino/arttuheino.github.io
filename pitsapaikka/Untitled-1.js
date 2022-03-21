d53d1b2dfaa932930e45e2088083eb1091468945
georgikeuda <99177979+georgikeuda@users.noreply.github.com>
2022-03-21 12:36:18 +0200

    tilaus sivusto

---
+++ pitsapaikka/Untitled-1.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", onkoLahettanut);
+
+function onkoLahettanut() {
+    let lahettanut = 'kylla';
+    if (lahettanut === 'kylla') {
+        document.getElementById().textContent += " Nimi";
+    }
+}
+
+function laheta() {
+    localStorage.setItem("puh", "nimi");
+}
---
+++ pitsapaikka/javas.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", onkoPuh);
+
+function onkoPuh() {
+    let puh = 'kylla';
+    if (puh === 'kylla') {
+        document.getElementById("puh_lomake").textContent += " Nimi";
+    }
+}
